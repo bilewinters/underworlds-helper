44c1408170ff17e3eaeeddd226e71e62d842faea
Paul McCabe <paul.mccabe@anaplan.com>
2019-05-25 23:39:52 +0100

    DebugginDebugging issue  with android version code

--- updateVersionNumber.js
+++ updateVersionNumber.js
@@ -3,5 +3,6 @@ const appInfo = require('./app.json');
 
 appInfo.expo.ios.buildNumber = `${process.argv[2]}`;
 appInfo.expo.android.versionCode = parseInt(`${process.argv[2]}`, 10);
+console.log(appInfo.expo.android.versionCode);
 
 fs.writeFileSync('./app.json', JSON.stringify(appInfo));
