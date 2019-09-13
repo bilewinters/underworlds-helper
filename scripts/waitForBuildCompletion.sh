2648dcaabbf35d68c332d95ff24dd43cf268860c
Paul McCabe <paul.mccabe@anaplan.com>
2019-09-13 23:24:03 +0100

    Fixing scripts

--- scripts/waitForBuildCompletion.sh
+++ scripts/waitForBuildCompletion.sh
@@ -4,7 +4,7 @@ timeout=60
 
 echo "Checking for build ${buildUrl}"
 
-while [ $(expo build:status | grep -A1 $buildUrl | grep -o "Build in progress...\|Build waiting in queue..." | wc -l) == "1" ]; do
+while [ $(node_modules/expo-cli/bin/expo.js build:status | grep -A1 $buildUrl | grep -o "Build in progress...\|Build waiting in queue..." | wc -l) == "1" ]; do
   timeout=$[ $timeout - 1 ]
   if [ $timeout -lt 1 ]; then
     echo "Build did not complete."
