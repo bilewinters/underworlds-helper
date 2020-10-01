3ef018c2c372e0d1ff4fa0afc2656fa63526660f
Paul McCabe <phmccabe@googlemail.com>
2020-10-01 23:44:48 +0100

    Adding functions for editing screenshots to add marketing text.

--- screenshot/screenshot.spec.js
+++ screenshot/screenshot.spec.js
@@ -1,43 +1,47 @@
-import path from 'path';
-import { execSync } from 'child_process';
-import { mkdirSync } from 'fs';
-import { takeScreenshot } from './screenshot.utils';
+import path from "path";
+import { execSync } from "child_process";
+import { mkdirSync } from "fs";
+import { takeScreenshot } from "./screenshot.utils";
 
-import menuScreenModel from './screenModels/MenuScreenModel';
-import gameScreenModel from './screenModels/GameScreenModel';
-import summaryScreenModel from './screenModels/SummaryScreenModel';
+import menuScreenModel from "./screenModels/MenuScreenModel";
+import gameScreenModel from "./screenModels/GameScreenModel";
+import summaryScreenModel from "./screenModels/SummaryScreenModel";
 
-const wdio = require('webdriverio');
+const wdio = require("webdriverio");
 
 const appLocation = process.env.APP_LOCATION;
 const device = process.env.DEVICE_NAME;
 const shouldExpoInstall = Boolean(process.env.SHOULD_EXPO_INSTALL);
-const root = path.resolve(path.join(__dirname, '../'));
-const screenshotsDirectory = path.resolve(path.join(root, 'marketing/screenshots'));
+const root = path.resolve(path.join(__dirname, "../"));
+const screenshotsDirectory = path.resolve(
+  path.join(root, "marketing/screenshots")
+);
 const imagesDirectory = path.resolve(path.join(screenshotsDirectory, device));
-const imagePath = imageName => path.resolve(path.join(imagesDirectory, imageName));
+const imagePath = (imageName) =>
+  path.resolve(path.join(imagesDirectory, imageName));
 
 const opts = {
   port: 4723,
+  path: "/wd/hub",
   capabilities: {
-    platformName: 'iOS',
+    platformName: "iOS",
     maxInstances: 1,
-    'appium:deviceName': device,
-    'appium:platformVersion': '13.0',
-    'appium:orientation': 'PORTRAIT',
-    'appium:automationName': 'XCUITest',
-    'appium:app': appLocation,
-    'appium:noReset': true,
-    'appium:newCommandTimeout': 240,
+    "appium:deviceName": device,
+    "appium:platformVersion": "13.5",
+    "appium:orientation": "PORTRAIT",
+    "appium:automationName": "XCUITest",
+    "appium:app": appLocation,
+    "appium:noReset": true,
+    "appium:newCommandTimeout": 240,
   },
 };
 
 const player1 = 0;
 const player2 = 1;
 
-const onePlayerFlow = async client => {
+const onePlayerFlow = async (client) => {
   const menuScreen = await menuScreenModel(client);
-  await takeScreenshot(client, imagePath('1-01-menu.png'));
+  await takeScreenshot(client, imagePath("1-01-menu.png"));
   await menuScreen.clickOnePlayerButton();
 
   await client.pause(2000);
@@ -45,7 +49,7 @@ const onePlayerFlow = async client => {
   await gameScreen.flipActivationsForPlayer(player1, 2);
   await gameScreen.addGloryToPlayer(player1, 3);
   await gameScreen.flipGloryForPlayer(player1, 0);
-  await takeScreenshot(client, imagePath('1-02-OnePlayerRoundOne.png'));
+  await takeScreenshot(client, imagePath("1-02-OnePlayerRoundOne.png"));
   await gameScreen.flipActivationsForPlayer(player1, 2);
   await gameScreen.endRound();
 
@@ -54,7 +58,7 @@ const onePlayerFlow = async client => {
   await gameScreen.flipActivationsForPlayer(player1, 1);
   await gameScreen.addGloryToPlayer(player1, 6);
   await gameScreen.flipGloryForPlayer(player1, 1, 2, 5, 6);
-  await takeScreenshot(client, imagePath('1-03-OnePlayerRoundTwo.png'));
+  await takeScreenshot(client, imagePath("1-03-OnePlayerRoundTwo.png"));
   await gameScreen.flipActivationsForPlayer(player1, 3);
   await gameScreen.endRound();
 
@@ -62,16 +66,16 @@ const onePlayerFlow = async client => {
   await gameScreen.flipActivationsForPlayer(player1, 4);
   await gameScreen.addGloryToPlayer(player1, 4);
   await gameScreen.flipGloryForPlayer(player1, 3, 4, 10);
-  await takeScreenshot(client, imagePath('1-04-OnePlayerRoundThree.png'));
+  await takeScreenshot(client, imagePath("1-04-OnePlayerRoundThree.png"));
   await gameScreen.endRound();
 
   await client.pause(2000);
   const summaryScreen = await summaryScreenModel(client);
-  await takeScreenshot(client, imagePath('1-05-OnePlayerSummary.png'));
+  await takeScreenshot(client, imagePath("1-05-OnePlayerSummary.png"));
   await summaryScreen.toMenu();
 };
 
-const twoPlayerFlow = async client => {
+const twoPlayerFlow = async (client) => {
   const menuScreen = await menuScreenModel(client);
   await menuScreen.clickTwoPlayerButton();
 
@@ -82,7 +86,7 @@ const twoPlayerFlow = async client => {
   await gameScreen.addGloryToPlayer(player1, 4);
   await gameScreen.flipGloryForPlayer(player1, 0);
   await gameScreen.addGloryToPlayer(player2, 2);
-  await takeScreenshot(client, imagePath('2-01-TwoPlayerRoundOne.png'));
+  await takeScreenshot(client, imagePath("2-01-TwoPlayerRoundOne.png"));
   await gameScreen.flipActivationsForPlayer(player1, 2);
   await gameScreen.flipActivationsForPlayer(player2, 3);
   await gameScreen.endRound();
@@ -95,7 +99,7 @@ const twoPlayerFlow = async client => {
   await gameScreen.flipGloryForPlayer(player1, 1, 2, 3, 7, 8);
   await gameScreen.addGloryToPlayer(player2, 4);
   await gameScreen.flipGloryForPlayer(player2, 0, 1, 2);
-  await takeScreenshot(client, imagePath('2-02-TwoPlayerRoundTwo.png'));
+  await takeScreenshot(client, imagePath("2-02-TwoPlayerRoundTwo.png"));
   await gameScreen.flipActivationsForPlayer(player1, 2);
   await gameScreen.flipActivationsForPlayer(player2, 1);
   await gameScreen.endRound();
@@ -108,19 +112,19 @@ const twoPlayerFlow = async client => {
   await gameScreen.flipGloryForPlayer(player1, 4, 9);
   await gameScreen.addGloryToPlayer(player2, 6);
   await gameScreen.flipGloryForPlayer(player2, 3, 4, 5, 6, 7, 8);
-  await takeScreenshot(client, imagePath('2-03-TwoPlayerRoundThree.png'));
+  await takeScreenshot(client, imagePath("2-03-TwoPlayerRoundThree.png"));
   await gameScreen.endRound();
 
   await client.pause(2000);
   const summaryScreen = await summaryScreenModel(client);
-  await takeScreenshot(client, imagePath('2-04-TwoPlayerSummary.png'));
+  await takeScreenshot(client, imagePath("2-04-TwoPlayerSummary.png"));
   await summaryScreen.toMenu();
 };
 
-test('Perform Screenshots', async () => {
+test("Perform Screenshots", async () => {
   const client = await wdio.remote(opts);
   if (shouldExpoInstall) {
-    execSync('node_modules/expo-cli/bin/expo.js ios', { cwd: root });
+    execSync("node_modules/expo-cli/bin/expo.js ios", { cwd: root });
   }
   mkdirSync(imagesDirectory, { recursive: true });
   await onePlayerFlow(client);
--- screenshot/screenshot.utils.js
+++ screenshot/screenshot.utils.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-plusplus */
+import sharp from "sharp";
+
 export const getElement = async (client, selector, timeout = 10000) => {
   let timeoutCount = timeout / 1000;
   let element = await client.$(selector);
@@ -18,4 +20,49 @@ export const takeScreenshot = async (client, fileName) => {
   await client.pause(1500);
   await client.saveScreenshot(fileName);
   return client.pause(1500);
+};
+
+const createSvgTextImageBuffer = (text, width, height) => {
+  const paddingTop = height * 0.025;
+  const lines = text.split("\n");
+  const lineHeight = Math.floor((height * 0.175) / lines.length);
+  const halfLineHeight = Math.floor(lineHeight / 2);
+  return Buffer.from(
+    `<svg width="${width}" height="${height}">
+     ${lines.map(
+       (line, index) =>
+         `<text x="50%" y="${
+           lineHeight * (index + 1) - halfLineHeight + paddingTop
+         }" text-anchor="middle" font-size="100" fill="#fff">${line}</text>`
+     )}
+   </svg>`
+  );
+};
+
+export const addMarketingText = async (imageFile, text) => {
+  const originalImage = await sharp(imageFile);
+  const { width, height } = await originalImage.metadata();
+  const textBuffer = createSvgTextImageBuffer(text, width, height);
+  const resizedImageBuffer = await originalImage
+    .resize(Math.floor(width * 0.8), Math.floor(height * 0.8))
+    .toBuffer();
+
+  sharp({
+    create: {
+      width,
+      height,
+      channels: 4,
+      background: { r: 0, g: 0, b: 0, alpha: 1 },
+    },
+  })
+    .composite([
+      {
+        input: resizedImageBuffer,
+        gravity: "south",
+      },
+      {
+        input: textBuffer,
+      },
+    ])
+    .toFile(imageFile);
 };
