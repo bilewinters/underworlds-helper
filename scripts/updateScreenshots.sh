0d0104c5d8222f60eb20502337df871402a6e56d
Paul McCabe <phmccabe@googlemail.com>
2020-10-18 19:55:08 +0100

    Adjusting order of output to see if screenshots take place.

--- scripts/updateScreenshots.sh
+++ scripts/updateScreenshots.sh
@@ -79,10 +79,14 @@ echo "----------------------------------------"
 echo "------------- Appium Output ------------"
 echo "----------------------------------------"
 kill -9 $appiumPID
-cat appium.log
 ls -hal "marketing/screenshots/iPhone 11 Pro Max"
 ls -hal "marketing/screenshots/iPhone 8 Plus"
 ls -hal "marketing/screenshots/iPad Pro (12.9-inch) (3rd generation)"
 
 echo "Not actually doing anything with these screenshots currently!"
 echo "You should probably upload them somewhere :-)"
+
+echo "----------------------------------------"
+echo "-------------- Appium Logs -------------"
+echo "----------------------------------------"
+cat appium.log
