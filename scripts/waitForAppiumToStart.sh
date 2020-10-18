0ecbd0a93288a572ee8df5ff3eff40b483bd10bb
Paul McCabe <phmccabe@googlemail.com>
2020-10-18 13:25:53 +0100

    Modifying script that waits for appium to start

--- scripts/waitForAppiumToStart.sh
+++ scripts/waitForAppiumToStart.sh
@@ -2,15 +2,18 @@
 logFile=$1
 timeout=60
 
-echo "Waiting for Appium to start..."
-while [ $([ -f "${logFile}" ] && grep "Appium REST http interface listener started" ${logFile} | wc -l) == "0" ]; do
+echo "Waiting for Appium to start, checking ${logFile}..."
+
+while [ $(grep -A1 "Appium REST http interface listener started" ${logFile} | wc -l) == "0" ]; do
   timeout=$[ $timeout - 1 ]
   if [ $timeout -lt 1 ]; then
-    echo "Appium not yet started"
+    echo "Appium not yet started."
+    cat ${logFile}
     exit -1
   fi
   echo "Waiting for Appium to start..."
   sleep 30
 done
 echo "Appium started!"
+cat ${logFile}
 exit 0
